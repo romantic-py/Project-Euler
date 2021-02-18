3c34d1451cf086b5bb52ac9a99ba0e200ea40fdf
Nabin Sahoo <79052753+romantic-py@users.noreply.github.com>
2021-02-18 22:24:13 +0530

    Update pe2.py

--- python/pe2.py
+++ python/pe2.py
@@ -2,7 +2,10 @@ fib = [1, 1]
 answer = 0
 while fib[1] < 4 * 10 ** 6:
     fib = [fib[1], fib[0] + fib[1]]
+    # we just need the last two terms to generate the next term.
+    # so we discard the first element of fib and append the next term.
     if fib[1] % 2 == 0:
+        # even
         answer += fib[1]
         
 print(answer)
