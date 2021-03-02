6c6c1f742a9ff496b1620f6fd578316fbbb072e1
Nabin Sahoo <79052753+romantic-py@users.noreply.github.com>
2021-03-02 21:14:50 +0530

    Update pe9.py

--- python/pe9.py
+++ python/pe9.py
@@ -2,7 +2,7 @@ answer = 0
 for c in range(999, 3, -1):
     if answer > 0:
         break
-    for b in range(1, max(1000 - c + 1, c)):
+    for b in range(2, max(1000 - c + 1, c)):
         a = 1000 - b - c
         if a*a + b*b == c*c:
             answer = a*b*c
