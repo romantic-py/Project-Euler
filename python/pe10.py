4a09ce5a8581067592cd52dcf993179e098895fa
Nabin Sahoo <79052753+romantic-py@users.noreply.github.com>
2021-03-02 21:21:14 +0530

    Create pe10.py

---
+++ python/pe10.py
@@ -0,0 +1,29 @@
+# original algorithm to generate prime list upto a certain integer.
+# modified from pe7.py
+
+primes = [2, 3, 5, 7]
+
+def check_primality(n, int_root):
+    for p in primes:
+        if p > int_root:  # p > n ** 0.5
+            break
+        elif n % p == 0:  # n is not prime
+            return False
+    return True
+
+def extend_prime_list(upto_number = 2 * 10 ** 6):
+    n = 11
+    last_int_root = 4
+    while n < upto_number:
+        if last_int_root * last_int_root <= n:
+            last_int_root += 1
+        if check_primality(n, last_int_root):
+            primes.append(n)
+        n += 2
+    return None
+
+
+if __name__ == "__main__":
+    extend_prime_list()
+    answer = sum(primes)
+    print(answer)
