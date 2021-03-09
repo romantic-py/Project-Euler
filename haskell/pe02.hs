dbb25e9b09d2d7402b6a072ce62e1132c8390d73
Nabin Sahoo <79052753+romantic-py@users.noreply.github.com>
2021-03-10 01:07:58 +0530

    rename function and the file name

---
+++ haskell/pe02.hs
@@ -0,0 +1,8 @@
+make_fibonacci_n fib n  -- generates the list of fibonacci numbers till the first such number >= n
+    | head fib > n = fib
+    | otherwise = make_fibonacci_n (sum(take 2 fib) : fib) n
+
+fib = make_fibonacci_n [2, 1] (4 * 10^6)
+
+answer = sum $ filter even $ tail fib
+main = putStrLn (show answer)
